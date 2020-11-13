6f315a51d4fab735d10176b8ad0ece224bc75e45
s0hv <tariq.harb@tuni.fi>
2020-11-13 17:57:30 +0200

    Tests for isEmpty

---
+++ test/isEmpty.test.js
@@ -0,0 +1,18 @@
+import isEmpty from '../src/isEmpty.js';
+
+describe('isEmpty', () => {
+    it('Should return true with an empty value', () => {
+        expect(isEmpty(null)).to.be.true;
+        expect(isEmpty(true)).to.be.true;
+        expect(isEmpty(1)).to.be.true;
+        expect(isEmpty({})).to.be.true;
+        expect(isEmpty('')).to.be.true;
+    });
+
+    it('Should return false with non empty values', () => {
+        expect(isEmpty({ a : 1 })).to.be.false;
+        expect(isEmpty('a')).to.be.false;
+        expect(isEmpty([1])).to.be.false;
+    });
+});
+
