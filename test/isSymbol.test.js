97fa6dc80f19cda7874f26600b00440f8f171f61
s0hv <tariq.harb@tuni.fi>
2020-11-30 14:42:13 +0200

    Add missing value to test case

--- test/isSymbol.test.js
+++ test/isSymbol.test.js
@@ -5,6 +5,7 @@ describe('isSymbol', () => {
     it('Should return false with plain object and null', () => {
         expect(isSymbol({})).to.be.false;
         expect(isSymbol(null)).to.be.false;
+        expect(isSymbol('abc')).to.be.false;
     });
 
     it('Should return true with a symbol', () => {
